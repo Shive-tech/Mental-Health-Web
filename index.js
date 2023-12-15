a4be99499615f6644b679834e5a5c4586bf7f872
uglychilde <147838961+uglychilde@users.noreply.github.com>
2023-12-15 09:50:01 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,17 +3,26 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
 
-function scaleWebpage() {
-  // Adjust the divisor and other values as needed
-  var scaleFactor = window.innerWidth / 800;
-  document.documentElement.style.fontSize = scaleFactor * 16 + 'px';
+function scalePage() {
+  // Get the current window width
+  var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+  // Set a base font size
+  var baseFontSize = 16;
+
+  // Calculate a scaling factor based on the window width
+  var scaleFactor = windowWidth / 1200; // You can adjust 1200 to your desired base width
+
+  // Adjust the font size
+  var newFontSize = baseFontSize * scaleFactor;
+  document.body.style.fontSize = newFontSize + 'px';
+
+  // You can add additional scaling logic for other elements if needed
 }
 
-// Initial scaling on page load
-scaleWebpage();
-
-// Scaling on window resize
-window.addEventListener('resize', scaleWebpage);
+// Call the function on page load and resize
+window.addEventListener('load', scalePage);
+window.addEventListener('resize', scalePage);
 
 
   // function scaleWebsite() {
